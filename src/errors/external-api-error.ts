02d7075911abc8ad83751435593292473f290763
Ezzu <ertizaejaz@yahoo.com>
2026-01-11 16:40:47 +0500

    [BE] Replace 502 reserved error for deployed services with 503

--- src/errors/external-api-error.ts
+++ src/errors/external-api-error.ts
@@ -2,7 +2,7 @@ import { ApiError } from './api-error';
 
 export class ExternalApiError extends ApiError {
   constructor(message: string, code?: string) {
-    super(message, 502, code);
+    super(message, 503, code);
     this.name = 'ExternalApiError';
   }
 }
